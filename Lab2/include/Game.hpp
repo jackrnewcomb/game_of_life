3a8bce065925c3e28e020f9e21dafe3f27bae762
Jack Newcomb <jackrnewcomb@gmail.com>
2025-09-29 11:19:33 -0400

    Function commenting

--- Lab2/include/Game.hpp
+++ Lab2/include/Game.hpp
@@ -1,3 +1,15 @@
+/*
+Author: Jack Newcomb
+Class: ECE6122
+Last Date Modified: 09/29/2025
+
+Description:
+
+Provides a class that handles all "visual" elements of the game. This includes sf::Sprite maintenance, drawing, display,
+etc. Also owns a shared pointer to the Grid
+
+*/
+
 #pragma once
 #include <SFML/Graphics.hpp>
 
@@ -6,28 +18,54 @@
 class Game
 {
   public:
+    /**
+     * @brief Constructor
+     * @param xWin: The number of pixels contained by the window in the X axis
+     * @param yWin: The number of pixels contained by the window in the Y axis
+     * @param cellSize: The width/height, in pixels, of cells
+     */
     Game(int xWin, int yWin, int cellSize);
+
+    /**
+     * @brief Performs an update loop for the game. This involves updating sprites with the new Grid information,
+     * re-drawing, and re-displaying
+     */
     void update();
+
+    /**
+     * @brief Returns a bool that describes the current status of the game (running or not running)
+     * @returns True if running, false if not
+     */
     bool isRunning()
     {
         return isRunning_;
     }
 
+    /**
+     * @brief Getter
+     * @returns Returns a shared pointer to the Grid
+     */
     std::shared_ptr<Grid> getGrid()
     {
         return grid_;
     }
 
   private:
+    // A shared pointer to the sf::VideoMode
     std::shared_ptr<sf::VideoMode> vm_;
 
+    // A shared pointer to the sf::RenderWindow
     std::shared_ptr<sf::RenderWindow> window_;
 
+    // A map of textures (allows us to avoid re-loading textures constantly)
     std::map<std::string, sf::Texture> textures_;
 
+    // A shared pointer to the Grid
     std::shared_ptr<Grid> grid_;
 
+    // The height/width of the square cell (total area = cellSize_ * cellSize_)
     int cellSize_;
 
+    // A bool tracking the status of the game (running or not running)
     bool isRunning_{true};
 };
--- Lab2/include/Grid.hpp
+++ Lab2/include/Grid.hpp
@@ -1,3 +1,14 @@
+/*
+Author: Jack Newcomb
+Class: ECE6122
+Last Date Modified: 09/29/2025
+
+Description:
+
+Provides a class that handles the heavy computational work involved in updating cells based on the rules of the game.
+
+*/
+
 #pragma once
 
 #include <iostream>
@@ -9,35 +20,94 @@
 class Grid
 {
   public:
+    /**
+     * @brief Constructor
+     * @param xLen: The number of elements (cells) contained in the X axis
+     * @param yLen: The number of elements (cells) contained in the Y axis
+     */
     Grid(int xLen, int yLen);
+
+    /**
+     * @brief Updates the cell located at coordinates (i,j)
+     * @param i: The row indicator of the cell
+     * @param j: The column indicator of the cell
+     */
     void updateCell(int i, int j);
+
+    /**
+     * @brief Updates all cells sequentially using a single thread
+     */
     void updateSEQ();
+
+    /**
+     * @brief Updates all cells using std::thread multi-threading
+     * @param numThreads: The number of threads to use
+     */
     void updateTHRD(int numThreads);
+
+    /**
+     * @brief Updates all cells using OpenMP
+     */
     void updateMP();
+
+    /**
+     * @brief Getter
+     * @returns Returns the cells_ array
+     */
     std::vector<char> getCells()
     {
         return cells_;
     }
 
+    /**
+     * @brief Getter
+     * @returns Returns the number of rows
+     */
     int getRows()
     {
         return rows_;
     }
+
+    /**
+     * @brief Getter
+     * @returns Returns the number of columns
+     */
     int getCols()
     {
         return cols_;
     }
 
+    /**
+     * @brief An indexer. Since we're using a flattened, 1D vector to store cell status, this takes in a row and column
+     * and provides the actual index of the cells_ vector
+     * @param i: The desired row
+     * @param j: The desired column
+     * @returns Index given row and column
+     */
     int index(int i, int j)
     {
         return i * cols_ + j;
     }
 
   private:
+    /**
+     * @brief RNG function that provides a "random" assignment to each cell to start
+     * @returns A char describing status ("0" for dead, "1" for alive")
+     */
     char randomStart();
+
+    // The number of rows in the grid
     int rows_;
+
+    // The number of columns in the grid
     int cols_;
+
+    // The flattened 1D vector of cells
     std::vector<char> cells_;
+
+    // The "next" vector after the update (required to avoid overlapping cell statuses)
     std::vector<char> newCells_;
+
+    // Random Number Generator
     std::default_random_engine generator;
 };
