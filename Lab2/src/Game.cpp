0c0150b6d06b082b64ab97df6b71b28659563ae7
Jack Newcomb <jackrnewcomb@gmail.com>
2025-09-30 10:09:48 -0400

    more cleanup and commenting

--- Lab2/src/Game.cpp
+++ Lab2/src/Game.cpp
@@ -2,23 +2,20 @@
 
 Game::Game(int xWin, int yWin, int cellSize)
 {
+    // Initialize members
     vm_ = std::make_shared<sf::VideoMode>(xWin, yWin);
     window_ = std::make_shared<sf::RenderWindow>(*vm_, "Game of Life", sf::Style::Default);
     cellSize_ = cellSize;
     grid_ = std::make_shared<Grid>(xWin / cellSize, yWin / cellSize);
 
-    int xPixelsPerCell = cellSize;
-    int yPixelsPerCell = cellSize;
-
-    int runningX = 0;
-    int runningY = 0;
-
+    // Populate the textures_ map so we only have to load textures once
     textures_["dead"].loadFromFile("graphics/dead.png");
     textures_["alive"].loadFromFile("graphics/alive.png");
 }
 
 void Game::update()
 {
+    // If the user presses escape, close the window
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
     {
         isRunning_ = false;
@@ -28,18 +25,33 @@ void Game::update()
     // Redraw the map with new movements and entity updates
     window_->clear();
 
+    // Get the cells from our grid
     auto &cells = grid_->getCells();
+
+    // For each row and column in the grid...
     for (int i = 0; i < grid_->getRows(); i++)
     {
         for (int j = 0; j < grid_->getCols(); j++)
         {
+            // Make a sprite
             sf::Sprite sprite;
-            sprite.setTexture(cells[grid_->index(i, j)] ? textures_["alive"] : textures_["dead"]);
-            sprite.setPosition(i * cellSize_, j * cellSize_);
-            sprite.setScale(cellSize_ / 100.f, cellSize_ / 100.f); //???
+
+            // Set the texture to either alive (white) or dead (black) depending on whether the grid element is alive or
+            // dead
+            sprite.setTexture(cells[grid_->index(i, j)] == 1 ? textures_["alive"] : textures_["dead"]);
+
+            // Set the position to be the location of the row and column, factoring in cell size
+            sprite.setPosition(static_cast<float>(i * cellSize_), static_cast<float>(j * cellSize_));
+
+            // Set the scale dependent on cell size and texture size
+            sprite.setScale(cellSize_ / static_cast<float>(sprite.getTexture()->getSize().x),
+                            cellSize_ / static_cast<float>(sprite.getTexture()->getSize().y));
+
+            // Draw the sprite
             window_->draw(sprite);
         }
     }
 
+    // Display after drawing all sprites
     window_->display();
 }
